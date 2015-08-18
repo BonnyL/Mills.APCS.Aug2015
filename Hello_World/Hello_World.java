bd16f8279be0cb365928050d4beebe8bebb9233d
BonnyL <lee.bonny98@gmail.com>
2015-08-18 09:21:52 -0700

    8.18.15 "Hello world demonstration"
    this was okay.

---
+++ Hello_World/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is our first Java code.
+ * 
+ * @author Bonny 
+ * @version (a version number or a date)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Bonny");
+        //Display the string
+    }
+}
