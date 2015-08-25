8247d0c1e563c4c65c3958742d919f8030307047
BonnyL <lee.bonny98@gmail.com>
2015-08-25 09:31:39 -0700

    Mississippi
    hw

---
+++ Mississippi/Mississippi.java
@@ -0,0 +1,28 @@
+public class Mississippi
+{
+    public static void Em() {
+        System.out.println("M     M\nMM   MM\nM M M M\nM  M  M\nM     M\nM     M\nM     M");
+    }
+    public static void Eye() {
+        System.out.println("IIIII\n  I\n  I\n  I\n  I\n  I\nIIIII");
+    }
+    public static void Ess() {
+        System.out.println(" SSSSS\nS     S\nS\n SSSSS\n      S\nS     S\n SSSSS");
+    }
+    public static void Pee() {
+        System.out.println("PPPPPP\nP     P\nP     P\nPPPPPP\nP\nP\nP");
+    }
+    public static void main(String[] args) {
+        Em();
+        Eye();
+        Ess();
+        Ess();
+        Eye();
+        Ess();
+        Ess();
+        Eye();
+        Pee();
+        Pee();
+        Eye();
+    }
+}
